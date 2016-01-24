0682899b5c9443900b7ed1b17a43c1f6d7183471
Carlos E. Palha <carlao_epalha@hotmail.com>
2016-01-24 16:49:00 -0200

    Branch jobs_by_category done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :jobs, only: [:show, :new, :create]
   resources :companies, only: [:new, :create, :show]
+  resources :categories, only: [:new, :create, :show]
+
 end

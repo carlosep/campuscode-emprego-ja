97bd58af4b0ea89d0c9c19d2bd5ec013778dda84
Carlos E. Palha <carlao_epalha@hotmail.com>
2016-01-25 10:52:10 -0200

    Branch recent_jobs done

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,12 @@ class Job < ActiveRecord::Base
   belongs_to :category
 
   validates :title, :category, :description, :location, presence: true
+
+  def recent?
+    if created_at > 5.days.ago
+      true
+    else
+      false
+    end
+  end
 end

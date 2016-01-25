56aea123e1ff7961302ed88d0d98ae6ae1245b28
Carlos E. Palha <carlao_epalha@hotmail.com>
2016-01-25 00:41:14 -0200

    Branch user_create_categories done

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,23 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category
+    else
+      @errors = @category.errors
+      render :new
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end

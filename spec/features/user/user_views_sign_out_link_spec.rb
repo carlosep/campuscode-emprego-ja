d259481e77b9850265036a9ae8ba23362a2c8c39
Carlos E. Palha <carlao_epalha@hotmail.com>
2016-01-30 17:43:39 -0200

    Added more tests

---
+++ spec/features/user/user_views_sign_out_link_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'User views sign out link' do
+  scenario 'successfully' do
+    visit root_path
+    click_on "Sign in"
+    sign_in
+    expect(page).to have_content("Sign out")
+  end
+  scenario 'unsuccessfully' do
+    visit root_path
+    expect(page).to have_content("Sign in")
+    expect(page).to have_content("Sign up")
+  end
+end

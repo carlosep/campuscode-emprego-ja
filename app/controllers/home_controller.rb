55e85f070ac0af601b91003b31c193d807e6030a
Carlos E. Palha <carlao_epalha@hotmail.com>
2016-03-08 16:40:13 -0300

    Added Navbar and Sidebar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
     @jobs = Job.all
-    @companies = Company.all
-    @categories = Category.all
-    @contracts = Contract.all
+    $companies = Company.all
+    $categories = Category.all
+    $contracts = Contract.all
   end
 end

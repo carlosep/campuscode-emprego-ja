9aff1f994dbfddd809079df79cf41a4438e842f3
Carlos E. Palha <carlao_epalha@hotmail.com>
2016-03-13 08:28:08 -0300

    Fixed global variables error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_globals
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def set_globals
+    @companies = Company.all
+    @categories = Category.all
+    @contracts = Contract.all
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
   def index
     @jobs = Job.all
-    $companies = Company.all
-    $categories = Category.all
-    $contracts = Contract.all
   end
 end
